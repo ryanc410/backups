e1e72136b191a3739ec519e76c0c7b13b7494bbf
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-09-13 02:09:26 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,74 +1,48 @@
-#!/bin/bash
-########################################################################################
-# Author: Ryan Cook https://github.com/ryanc410                                        #
-#                                                                                      #
-# --DESCRIPTION--                                                                      #                                                          
-# -Provide arguments to customize what you want to backup.                             #
-# -If no arguments are provided, script uses a default set of directories to backup.   #
-#                                                                                      #
-# SYNTAX                                                                               #
-#                                                                                      #
-# sudo ./backup "/dir1 /dir2 /dir3....."                                               #
-#                                                                                      #
-########################################################################################
+#!/usr/bin/env bash
 
-###################################
-# VARIABLES
-##################################
+MONTH=$(date +%b)
+YEAR=$(date +%Y)
+BACKUP_DIRS=( "$@" )
+BACKUP_DEST=/home/backups/"$YEAR"/"$MONTH"
+FILENAME=$(hostname -s).tar.gz
 
-# Archive Name
-DAY=$(date +%m.%d.%y)
-SYS_NAME=$(hostname -s)
-ARCHIVE=${SYS_NAME}${DAY}.tgz
-
-# Files/Directories
-BU_FILES=( "$@" )
-DEFAULT_FILES=( "/etc" "/home" "/root" )
-BU_DEST=/home/backups
-
-
-###################################
-# FUNCTIONS
-##################################
-
-check_dir() {
-    if [[ ! -d ${BU_DEST} ]]; then
-        mkdir -p ${BU_DEST} &>/dev/null
-    fi
-}
-status() {
-    if [[ $? -eq 0 ]]; then
-        echo "Backup Completed Successfully!"
-        sleep 2
-        exit 0
-    else
-        echo "There was a problem completing the backup..."
-        sleep 2
-        exit 4
-    fi
-}
-
-###################################
-# SCRIPT
-##################################
-
-if [[ $EUID -ne 0 ]]; then
-    clear
-    echo "*****    SCRIPT MUST HAVE ROOT PRIVILEGES    *****"
+checkroot()
+{
+if [[ $EUID != 0 ]]; then
+    clear 
+    echo "Must be root to run this script!"
     sleep 2
-    exit 3
+    exit 1
 fi
-if [[ $@ == "" ]]; then
-    echo "No directories were provided."
-    echo ""
-    echo "Backing up default directories."
-    sleep 2
-    check_dir
-    tar czf ${BU_DEST}/${ARCHIVE} ${DEFAULT_FILES}
-    status
+}
+checkdest()
+{
+if [[ ! -d $BACKUP_DEST ]]; then
+    mkdir -p "$BACKUP_DEST"
+fi
+}
+checkargs()
+{
+if [[ $@ == 0 ]]; then
+    echo "ERROR: No arguments provided to script."
+    exit 1
+fi
+}
+
+checkroot
+
+checkdest
+
+checkargs
+
+tar -czf "$BACKUP_DEST"/"$FILENAME" "${BACKUP_DIRS[@]}" &>/dev/null
+
+if [ -z "$(ls -A $BACKUP_DEST)" ]; then
+    echo "Backup Failed!"
+    sleep 3
+    exit 1
 else
-    echo "Starting backup.."
-    check_dir
-    tar czf ${BU_DEST}/${ARCHIVE} ${BU_FILES}
-    status
+    echo "Backup Succeeded!"
+    sleep 3
+    exit 0
 fi
